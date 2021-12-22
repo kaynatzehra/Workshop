cfe74672ec2c1e0e2ef8ecdd5bf69b56fb90f681
Kaynat Zehra <kaynatzehra23@gmail.com>
2021-12-22 08:39:04 +0530

    Added the file.java

---
+++ src/gambling/UC1.java
@@ -0,0 +1,12 @@
+package gambling;
+
+public class UC1 {
+	
+    public static final int INITIAL_BET_STAKE = 100;
+    public static final int BET_AMT = 1;
+
+    public static void main(String[] args) {
+        System.out.println(INITIAL_BET_STAKE);
+        System.out.println(BET_AMT);
+    }
+}
